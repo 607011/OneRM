f2918bfd7a0abbbfbf1cef58f50679d72a6df48d
Oliver Lau <oliver@ersatzworld.net>
2020-05-11 08:36:16 +0200

    Initial Commit

---
+++ 1RM/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  1RM
+//
+//  Created by Oliver Lau on 11.05.20.
+//  Copyright © 2020 Oliver Lau. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
