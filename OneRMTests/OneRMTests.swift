2d7376fa975763b8b413ef03d4cf05791daf03ed
Oliver Lau <oliver@ersatzworld.net>
2020-05-26 07:36:09 +0200

    Simplified formula tests.

--- OneRMTests/OneRMTests.swift
+++ OneRMTests/OneRMTests.swift
@@ -10,94 +10,51 @@ class OneRMTests: XCTestCase {
     let epsilon = 0.000000001
     let repRange = 1...12
 
+    /// This method is called before the invocation of each test method in the class.
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    /// This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func runFormulaTest(_ formula: OneRMFormula) {
+        for reps in repRange {
+            let oneRM = formula.oneRM(weight: weight, reps: reps)
+            let rm = formula.rm(for: reps, with: oneRM)
+            XCTAssert(weight.almostEquals(rm, epsilon: epsilon))
+            if reps == 1 {
+                XCTAssert(oneRM.almostEquals(rm, epsilon: epsilon))
+            }
+        }
     }
 
     func testBrzycki() {
-        let formula = Brzycki()
-        for reps in repRange {
-            let oneRM = formula.oneRM(weight: weight, reps: reps)
-            let rm = formula.rm(for: reps, with: oneRM)
-            debugPrint(reps, oneRM, rm)
-            XCTAssert(weight.almostEquals(rm, epsilon: epsilon))
-            if reps == 1 {
-                XCTAssert(oneRM.almostEquals(rm, epsilon: epsilon))
-            }
-        }
+        runFormulaTest(Brzycki())
     }
 
     func testEpley() {
-        let formula = Epley()
-        for reps in repRange {
-            let oneRM = formula.oneRM(weight: weight, reps: reps)
-            let rm = formula.rm(for: reps, with: oneRM)
-            XCTAssert(weight.almostEquals(rm, epsilon: epsilon))
-            if reps == 1 {
-                XCTAssert(oneRM.almostEquals(rm, epsilon: epsilon))
-            }
-        }
+        runFormulaTest(Epley())
     }
 
     func testMcGlothin() {
-        let formula = McGlothin()
-        for reps in repRange {
-            let oneRM = formula.oneRM(weight: weight, reps: reps)
-            let rm = formula.rm(for: reps, with: oneRM)
-            XCTAssert(weight.almostEquals(rm, epsilon: epsilon))
-            if reps == 1 {
-                XCTAssert(oneRM.almostEquals(rm, epsilon: epsilon))
-            }
-        }
+        runFormulaTest(McGlothin())
     }
 
     func testLombardi() {
-        let formula = Lombardi()
-        for reps in repRange {
-            let oneRM = formula.oneRM(weight: weight, reps: reps)
-            let rm = formula.rm(for: reps, with: oneRM)
-            XCTAssert(weight.almostEquals(rm, epsilon: epsilon))
-            if reps == 1 {
-                XCTAssert(oneRM.almostEquals(rm, epsilon: epsilon))
-            }
-        }
+        runFormulaTest(Lombardi())
     }
 
     func testMayhew() {
-        let formula = Mayhew()
-        for reps in repRange {
-            let oneRM = formula.oneRM(weight: weight, reps: reps)
-            let rm = formula.rm(for: reps, with: oneRM)
-            XCTAssert(weight.almostEquals(rm, epsilon: epsilon))
-            if reps == 1 {
-                XCTAssert(oneRM.almostEquals(rm, epsilon: epsilon))
-            }
-        }
+        runFormulaTest(Mayhew())
     }
 
     func testOConner() {
-        let formula = OConner()
-        for reps in repRange {
-            let oneRM = formula.oneRM(weight: weight, reps: reps)
-            let rm = formula.rm(for: reps, with: oneRM)
-            XCTAssert(weight.almostEquals(rm, epsilon: epsilon))
-            if reps == 1 {
-                XCTAssert(oneRM.almostEquals(rm, epsilon: epsilon))
-            }
-        }
+        runFormulaTest(OConner())
     }
 
     func testWathen() {
-        let formula = Wathen()
-        for reps in repRange {
-            let oneRM = formula.oneRM(weight: weight, reps: reps)
-            let rm = formula.rm(for: reps, with: oneRM)
-            XCTAssert(weight.almostEquals(rm, epsilon: epsilon))
-        }
+        runFormulaTest(Wathen())
     }
 
     func testClosedRange() {
@@ -109,9 +66,9 @@ class OneRMTests: XCTestCase {
         XCTAssert(range.clamp(value: repRange.upperBound + 1) == 12)
     }
 
-    func testPerformanceExample() {
-        measure {
-        }
-    }
+//    func testPerformanceExample() {
+//        measure {
+//        }
+//    }
 
 }
